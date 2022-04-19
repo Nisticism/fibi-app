5da7b3cc3b3b6b2d3aec8a16384e2eab69c8d6bc
hfoster00 <94389711+hfoster00@users.noreply.github.com>
2022-04-18 18:35:43 -0700

    frontend connection

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const path = require('path');
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../chessus-frontend/build')));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
